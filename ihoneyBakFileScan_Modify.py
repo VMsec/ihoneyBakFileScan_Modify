332eeac0f01ca87e4772d7d30f519a3cd224d023
Se7en <admin@se7ensec.cn>
2022-06-22 16:41:30 +0800

    Update ihoneyBakFileScan_Modify.py

--- ihoneyBakFileScan_Modify.py
+++ ihoneyBakFileScan_Modify.py
@@ -1,77 +1,36 @@
-# 2018.04.20 www.T00ls.net
-# __author__: ihoneysec
 # -*- coding: UTF-8 -*-
 
 import requests
 import logging
-from binascii import b2a_hex
-import threading
-from queue import Queue
 from argparse import ArgumentParser
 from copy import deepcopy
 from datetime import datetime
 from hurry.filesize import size
+from concurrent.futures import ThreadPoolExecutor
 
 requests.packages.urllib3.disable_warnings()
 
 logging.basicConfig(level=logging.WARNING, format="%(message)s")
 
-
-def vlun(q,df):
-    while q.empty() is not True:
-        urltarget = q.get()
-        #print(urltarget)
-        '''
-        rar_byte = '526172'
-        zip_byte = '504b03'
-        gz_byte = '1f8b080000000000000b'
-        mysqldump_byte = '2d2d204d7953514c'
-        phpmyadmin_byte = '2d2d207068704d794164'
-        navicat_byte = '2f2a0a204e6176696361'
-        adminer_byte = '2d2d2041646d696e6572'
-        other_byte = '2d2d202d2d2d2d2d2d2d'
-        navicat_MDT_byte = '2f2a0a4e617669636174'
-        tar_gz_byte = '1f8b0800'
-        '''
-        try:
-            r = requests.get(url=urltarget, headers=headers, timeout=timeout, allow_redirects=False, stream=True, verify=False)
-            #content = b2a_hex(r.raw.read(10)).decode()
-
-            if (r.status_code == 200)&('html' not in r.headers.get('Content-Type'))&('xml' not in r.headers.get('Content-Type'))&('json' not in r.headers.get('Content-Type'))&('javascript' not in r.headers.get('Content-Type')):
-                '''
-                rarsize = int(r.headers.get('Content-Length'))
-                if rarsize >= 1024000000:
-                    unit = int(rarsize) // 1024 // 1024 / 1000
-                    rarsize = str(unit) + 'G'
-                elif rarsize >= 1024000:
-                    unit = int(rarsize) // 1024 // 1024
-                    rarsize = str(unit) + 'M'
-                else:
-                    unit = int(rarsize) // 1024
-                    rarsize = str(unit) + 'K'
-                if content.startswith(rar_byte) or content.startswith(zip_byte) or content.startswith(gz_byte) or content.startswith(
-                        mysqldump_byte) or content.startswith(
-                        phpmyadmin_byte) or content.startswith(navicat_byte) or content.startswith(adminer_byte) or content.startswith(
-                    other_byte) or content.startswith(navicat_MDT_byte) or content.startswith(tar_gz_byte):
-                #if int(unit)>0:
-                '''
-                tmp_rarsize = int(r.headers.get('Content-Length'))
-                rarsize = str(size(tmp_rarsize))                
-                if (int(rarsize[0:-1])>0):
-                    logging.warning('[ success ] {}  size:{}'.format(urltarget, rarsize))
-                    with open(df, 'a') as f:
-                        try:
-                            f.write(str(urltarget) + '  ' + 'size:' + str(rarsize) + '\n')
-                        except:
-                            pass
-                else:                
-                    logging.warning('[ fail ] {}'.format(urltarget))
-            else:           
+def vlun(urltarget):
+    try:
+        r = requests.get(url=urltarget, headers=headers, timeout=timeout, allow_redirects=False, stream=True, verify=False)
+        if (r.status_code == 200)&('html' not in r.headers.get('Content-Type'))&('xml' not in r.headers.get('Content-Type'))&('json' not in r.headers.get('Content-Type'))&('javascript' not in r.headers.get('Content-Type')):
+            tmp_rarsize = int(r.headers.get('Content-Length'))
+            rarsize = str(size(tmp_rarsize))                
+            if (int(rarsize[0:-1])>0):
+                logging.warning('[ success ] {}  size:{}'.format(urltarget, rarsize))
+                with open(outputfile, 'a') as f:
+                    try:
+                        f.write(str(urltarget) + '  ' + 'size:' + str(rarsize) + '\n')
+                    except:
+                        pass
+            else:                
                 logging.warning('[ fail ] {}'.format(urltarget))
-        except Exception as e:
-            pass
-        q.task_done()
-
+        else:           
+            logging.warning('[ fail ] {}'.format(urltarget))
+    except Exception as e:
+        pass
 
 def urlcheck(target=None, ulist=None):
     if target is not None and ulist is not None:
@@ -88,11 +47,9 @@ def urlcheck(target=None, ulist=None):
                 ulist.append(line + '/')
         return ulist
 
-
 def dispatcher(url_file=None, url=None, max_thread=1, dic=None):
     urllist = []
     check_urllist = []
-    global q
     if url_file is not None and url is None:
         with open(str(url_file)) as f:
             while True:
@@ -107,7 +64,7 @@ def dispatcher(url_file=None, url=None, max_thread=1, dic=None):
     else:
         pass
 
-    with open(datefile, 'w'):
+    with open(outputfile, 'w'):
         pass
 
     for u in urllist:
@@ -132,7 +89,7 @@ def dispatcher(url_file=None, url=None, max_thread=1, dic=None):
 
         current_info_dic = deepcopy(dic)  # deep copy
         suffixFormat = ['.zip','.rar','.tar.gz','.tgz','.tar.bz2','.tar','.jar','.war','.7z','.bak','.sql','.gz','.sql.gz','.tar.tgz']
-        domainDic = [ucp, ucp.replace('.', ''), wwwhost, ucp.split('.', 1)[-1], www1[0], www1[1]]
+        domainDic = [ucp, ucp.replace('.', ''), ucp.replace('.', '_'), wwwhost, ucp.split('.', 1)[-1], (ucp.split('.', 1)[1]).replace('.', '_'), www1[0], www1[1]]
 
         for s in suffixFormat:
             for d in domainDic:
@@ -143,26 +100,25 @@ def dispatcher(url_file=None, url=None, max_thread=1, dic=None):
             check_urllist.append(url)
             print("[add check] "+url)
 
-    q = Queue() 
+    l=[]
+    p = ThreadPoolExecutor(max_thread)
     for url in check_urllist:
-        q.put(url)
-    for index in range(max_thread):
-        thread = threading.Thread(target=vlun, args=(q,datefile))
-        # thread.daemon = True
-        thread.start()
-    q.join()
+        obj = p.submit(vlun, url)
+        l.append(obj)
+    p.shutdown()
 
 
 if __name__ == '__main__':
-    usageexample = '\n       Example: python3.5 ihoneyBakFileScan -t 100 -f url.txt\n'
+    usageexample = '\n       Example: python3 ihoneyBakFileScan_Modify.py -t 100 -f url.txt\n'
     usageexample += '                '
-    usageexample += 'python3.5 ihoneyBakFileScan.py -u https://www.example.com/'
+    usageexample += 'python3 ihoneyBakFileScan_Modify.py -u https://www.example.com/'
 
     parser = ArgumentParser(add_help=True, usage=usageexample, description='A Website Backup File Leak Scan Tool.')
     parser.add_argument('-f', '--url-file', dest="url_file", help="Example: url.txt")
     parser.add_argument('-t', '--thread', dest="max_threads", nargs='?', type=int, default=1, help="Max threads")
     parser.add_argument('-u', '--url', dest='url', nargs='?', type=str, help="Example: http://www.example.com/")
     parser.add_argument('-d', '--dict-file', dest='dict_file', nargs='?', help="Example: dict.txt")
+    parser.add_argument('-o', '--output-file', dest="output_file", help="Example: result.txt")
 
     args = parser.parse_args()
     # Use the program default dictionary, Accurate scanning mode, Automatic dictionary generation based on domain name.
@@ -175,24 +131,24 @@ if __name__ == '__main__':
     for a in tmp_info_dic:
         for b in tmp_suffixFormat:
             info_dic.extend([a + b])
-
-    datefile = datetime.now().strftime('%Y%m%d_%H-%M-%S.txt')
+    
+    global outputfile
+    outputfile = args.output_file
 
     headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36", }
     timeout = 10
 
-
     try:
-        if args.dict_file:
+        if args.dict_file and outputfile:
             # Custom scan dictionary
             # This mode is not recommended for bulk scans. It is prone to false positives and can reduce program efficiency.
             custom_dict = list(set([i.replace("\n", "") for i in open(str(args.dict_file), "r").readlines()]))
             info_dic.extend(custom_dict)
-        if args.url:
+        if args.url and outputfile:
             dispatcher(url=args.url, max_thread=args.max_threads, dic=info_dic)
-        elif args.url_file:
+        elif args.url_file and outputfile:
             dispatcher(url_file=args.url_file, max_thread=args.max_threads, dic=info_dic)
         else:
-            print("[!] Please specify a URL, or URL file name.")
+            print("[!] Please specify a URL, or URL file name, or RESULT file name.")
     except Exception as e:
         print(e)
